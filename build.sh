2f6ce5b0e52d3c2eb775b8d516ee0827210f781f
zhengkeyan <zhengkeyan@bjca.org.cn>
2024-09-27 17:42:59 +0800

    改进脚本

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo build for Linux-amd64 ...
 GOOS=linux GOARCH=amd64 go build -tags "linux" -ldflags "-w -s" -o m4s-converter-amd64 main.go
-
+#
 echo build for Linux-arm64 ...
 GOOS=linux GOARCH=arm64 GOARM=8 go build -tags "linux" -ldflags "-w -s" -o m4s-converter-arm64 main.go
-
-
+# 压缩
+upx --lzma  m4s-converter-*
 
