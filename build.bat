66b16ba7f3febfc0dc94cdcce8f750fe63c83e98
zhengkeyan <zhengkeyan@bjca.org.cn>
2024-09-29 11:28:45 +0800

    up

--- build.bat
+++ build.bat
@@ -7,10 +7,11 @@ set "targetFile=common\version.go"
     echo var Version = "1.3.7"
 ) > "%targetFile%"
 ::
-echo build for windows ...
+echo build for windows-x86...
 set GOARCH=386
 go build -tags "windows" -ldflags "-w -s" -o m4s-converter-386.exe main.go
-:: x86的exe文件兼容64位系统
+:: x86的exe文件兼容64位系统，实际都使用32位程序即可
+echo build for windows-amd64...
 set GOARCH=amd64
 go build -tags "windows" -ldflags "-w -s" -o m4s-converter-amd64.exe main.go
 :: 压缩
