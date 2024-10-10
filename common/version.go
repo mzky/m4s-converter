e2402594f3655935a311d2b52877217bdf72d32b
zhengkeyan <zhengkeyan@bjca.org.cn>
2024-10-10 08:39:35 +0800

    更新版本

--- common/version.go
+++ common/version.go
@@ -2,6 +2,6 @@ package common
 
 var (
 	Version   = "1.3.8"
-	SourceVer = "b611fd7 @2024-09-29 16:53:17 +0800"
-	BuildTime = "2024-09-29 16:53:46 by go1.21.5 linux/amd64"
+	SourceVer = "cff0a4c @2024-10-10 15:54:48 +0800"
+	BuildTime = "2024-10-10 08:35:03 by go1.21.5 linux/amd64"
 )
