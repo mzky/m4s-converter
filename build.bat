6e3b807a4b213ccb12a96a73559ea58db01bd785
zhengkeyan <zhengkeyan@bjca.org.cn>
2024-10-11 13:57:03 +0800

    兼容Android导出的文件

--- build.bat
+++ build.bat
@@ -8,7 +8,8 @@ echo build for windows-amd64...
 set GOARCH=amd64
 go build -tags "windows" -ldflags "-w -s" -o m4s-converter-amd64.exe
 :: 压缩
-upx --lzma m4s-converter-*.exe
+upx --lzma m4s-converter-386.exe
+upx --lzma m4s-converter-amd64.exe
 ::
 echo build for windows-arm64...
 set GOARCH=arm64
