9043e87161a6318c1f680db01b880d31cb57d588
zhengkeyan <zhengkeyan@bjca.org.cn>
2024-11-13 14:11:57 +0800

    自定义GPAC路径

--- common/version.go
+++ common/version.go
@@ -2,6 +2,6 @@ package common
 
 var (
 	version   = "1.4.0"
-	sourceVer = "8a4445a @2024-10-11 12:24:34 +0800"
-	buildTime = "2024-10-11 13:52:54 by go1.23.1 linux/amd64"
+	sourceVer = "929aae0 @2024-11-13 14:03:53 +0800"
+	buildTime = "2024-11-13 14:07:27 by go1.20.14 linux/amd64"
 )
