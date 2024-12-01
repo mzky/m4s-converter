0f49cd48efef3e3a5636f7a418f6f938ffdf3380
zhengkeyan <zhengkeyan@bjca.org.cn>
2024-12-02 07:29:06 +0800

    优化

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: tags 支持linux、windows、macos
-echo build for windows-amd64...
-set GOARCH=amd64
-go build -tags "windows" -ldflags "-w -s" -o m4s-converter-amd64.exe
-:: 压缩
-upx --lzma m4s-converter-amd64.exe
