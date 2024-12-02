21d1a0a05d519b01e7e9670738605bf891940cab
zhengkeyan <zhengkeyan@bjca.org.cn>
2024-12-02 08:33:40 +0800

    优化

--- build.sh
+++ build.sh
@@ -21,9 +21,9 @@ GOOS=windows GOARCH=amd64 go build -tags "windows" -ldflags "-w -s" -o m4s-conve
 #GOOS=linux GOARCH=arm64 GOARM=7 go build -tags "linux" -ldflags "-w -s" -o m4s-converter-linux_arm64 main.go
 # 设置目标操作系统为darwin(MacOS)
 echo build for darwin-amd64...
-GOOS=darwin GOARCH=amd64 go build -tags "darwin" -o m4s-converter-darwin_amd64 main.go
+GOOS=darwin GOARCH=amd64 go build -tags "darwin" -ldflags "-w -s" -o m4s-converter-darwin_amd64 main.go
 #
 echo build for darwin-arm64...
-GOOS=darwin GOARCH=arm64 go build -tags "darwin" -o m4s-converter-darwin_arm64 main.go
+GOOS=darwin GOARCH=arm64 go build -tags "darwin" -ldflags "-w -s" -o m4s-converter-darwin_arm64 main.go
 # 压缩
 upx --lzma m4s-converter-amd64.exe m4s-converter-linux_amd64
--- common/version.go
+++ common/version.go
@@ -1,7 +1,7 @@
 package common
 
 var (
-	version   = "1.5.2"
-	sourceVer = "0f49cd4 @2024-12-02 07:29:06 +0800"
-	buildTime = "2024-12-02 07:32:57 by go1.20.6 linux/amd64"
+	version   = "1.5.3"
+	sourceVer = "822e25e @2024-12-02 07:42:01 +0800"
+	buildTime = "2024-12-02 08:31:09 by go1.23.1 linux/amd64"
 )
